cdc28ea62cfd49d5ebc4b941ea8423441bc4dbe3
refracta <58779799+refracta@users.noreply.github.com>
2023-03-08 17:31:23 +0900

    Release v1.0 (#2)
    * Initial commit
    
    * Add github action (#1)
    
    * Add build-document action
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Add translation
    
    * Minor fix
    
    * Add build-document-windows.yml
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Add translation
    
    * Add original tex file
    
    * Add original tex file
    
    * Minor fix
    
    * Add translation
    
    * Minor fix
    
    * Minor fix
    
    * Add translation
    
    * Add translation
    
    * Minor fix
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Minor fix
    
    * Add summary command
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Add build.sh
    
    * Add build.sh
    
    * Add build.sh
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Minor fix
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Add translation
    
    * Add translation

---
+++ build.ps1
@@ -0,0 +1,17 @@
+$pdflatex = If ($args.Length -Eq 0) {"pdflatex"} Else {"& `"$args`""}
+$currentPath = pwd
+function Run-Build($OutputDirectory, $JobName, $TeXFile, $FinalName){
+    iex $($pdflatex + " -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$OutputDirectory -aux-directory=auxil -jobname=`"$JobName`" $TeXFile")
+    cd $OutputDirectory
+    mv "$JobName.pdf" "$FinalName.pdf" -Force
+    cd $currentPath
+}
+
+(Get-Content -Path SRGANpaper_CVPR_Arxiv.tex -Raw) -Replace "\\newcommand\{\\summary\}\[1\]\{\}", "\newcommand{\summary}[1]{{\color{blue}\textit{요약: #1}}}" > SRGANpaper_CVPR_Arxiv-Korean-Summary.tex
+(Get-Content -Path SRGANpaper_CVPR_Arxiv.tex -Raw) -Replace "\\newcommand\{\\eng\}\[1\]\{\}", "\newcommand{\eng}[1]{`n`n#1`n`n}" > SRGANpaper_CVPR_Arxiv-EngKor.tex
+(Get-Content -Path SRGANpaper_CVPR_Arxiv-Korean-Summary.tex -Raw) -Replace "\\newcommand\{\\eng\}\[1\]\{\}", "\newcommand{\eng}[1]{`n`n#1`n`n}" > SRGANpaper_CVPR_Arxiv-EngKor-Summary.tex
+
+Run-Build out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv SRGAN-Korean
+Run-Build out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-Korean-Summary SRGAN-Korean-Summary
+Run-Build out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-EngKor SRGAN-EngKor
+Run-Build out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-EngKor-Summary.tex SRGAN-EngKor-Summary
---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cp SRGANpaper_CVPR_Arxiv.tex SRGANpaper_CVPR_Arxiv-Korean-Summary.tex
+sed -i 's/\\newcommand{\\summary}\[1\]{}/\\newcommand{\\summary}[1]{{\\color{blue}\\textit{요약: #1}}}/g' SRGANpaper_CVPR_Arxiv-Korean-Summary.tex
+cp SRGANpaper_CVPR_Arxiv.tex SRGANpaper_CVPR_Arxiv-EngKor.tex
+sed -i 's/\\newcommand{\\eng\}\[1\]{}/\\newcommand{\\eng}[1]{\n\n#1\n\n}/g' SRGANpaper_CVPR_Arxiv-EngKor.tex
+cp SRGANpaper_CVPR_Arxiv-Korean-Summary.tex SRGANpaper_CVPR_Arxiv-EngKor-Summary.tex
+sed -i 's/\\newcommand{\\eng\}\[1\]{}/\\newcommand{\\eng}[1]{\n\n#1\n\n}/g' SRGANpaper_CVPR_Arxiv-EngKor-Summary.tex
+
+runBuild(){
+    local outputDirectory=${1}
+    local jobname=${2}
+    local texFile=${3}
+    local finalName=${4}
+    local currentPath=$(pwd)
+    docker run -i \
+      -v miktex:/miktex/.miktex \
+      -v `pwd`:/miktex/work \
+      miktex/miktex \
+      pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$outputDirectory -aux-directory=auxil -jobname=$jobname $texFile
+    cd $outputDirectory
+    sudo mv $jobname.pdf $finalName.pdf
+    cd $currentPath
+}
+
+runBuild out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv SRGAN-Korean
+runBuild out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv SRGAN-Korean
+runBuild out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-Korean-Summary SRGAN-Korean-Summary
+runBuild out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-EngKor SRGAN-EngKor
+runBuild out SRGANpaper_CVPR_Arxiv SRGANpaper_CVPR_Arxiv-EngKor-Summary.tex SRGAN-EngKor-Summary
